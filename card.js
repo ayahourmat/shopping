1438ffc777fb0ee9f5811e62e5397ac956f4e47e
ayahourmat <135506307+ayahourmat@users.noreply.github.com>
2023-06-22 15:22:11 +0000

    Add files via upload

---
+++ card.js
@@ -0,0 +1,60 @@
+//  (".heart.fa").click (function () { $ (this).toggleClass ("fa-heart fa-heart-o"); }); 
+ 
+//  var select = document.getElementById('Qty');
+//  var value = select.options.value;
+//  var price = document.getElementById('price');
+//  if (value = two) {
+//     price = 50
+//  } 
+
+
+
+
+
+
+// //     var qtyInputs = document.querySelectorAll('.qty, .price');
+// //     qtyInputs.forEach(function(input) {
+// //       input.addEventListener('keyup', updateAmounts);
+// //       input.addEventListener('keypress', updateAmounts);
+// //       input.addEventListener('blur', updateAmounts);
+// //       input.addEventListener('change', updateAmounts);
+// //     });
+
+  
+// //   function updateAmounts() {
+// //     var sum = 0.0;
+// //     var rows = document.querySelectorAll('#myTable > tbody > tr');
+// //     rows.forEach(function(row) {
+// //       var qty = parseInt(row.querySelector('.qty').value);
+// //       var price = parseFloat(row.querySelector('.price').value);
+// //       var amount = qty * price;
+// //       sum += amount;
+// //       row.querySelector('.amount').textContent = amount;
+// //     });
+    
+// //     document.querySelector('.total').textContent = sum;
+// //   }
+  
+// //   var plusBtn = document.querySelectorAll(".cart-qty-plus");
+// //   var minusBtn = document.querySelectorAll(".cart-qty-minus");
+  
+// //   plusBtn.forEach(function(btn) {
+// //     btn.addEventListener('click', function() {
+// //       var input = this.parentElement.querySelector('.qty');
+// //       var qty = parseInt(input.value);
+// //       input.value = qty + 1;
+// //       updateAmounts();
+// //     });
+// //   });
+  
+// //   minusBtn.forEach(function(btn) {
+// //     btn.addEventListener('click', function() {
+// //       var input = this.parentElement.querySelector('.qty');
+// //       var qty = parseInt(input.value);
+// //       if (qty > 0) {
+// //         input.value = qty - 1;
+// //       }
+// //       updateAmounts();
+// //     });
+// //   });
+  
